dacccf9bcfa16fdac44909f6f9bd32d2692c1fa3
rkayastha98 <rkayastha@wpi.edu>
2019-05-13 03:11:16 -0400

    Initial commit

---
+++ app/src/main/java/com/example/myforethought/ui/login/dashboard/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.myforethought.ui.login.dashboard;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.myforethought.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+//    private TextView mTextMessage;
+//
+//    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+//            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+//
+//        @Override
+//        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//            switch (item.getItemId()) {
+//                case R.id.navigation_home:
+//                    mTextMessage.setText(R.string.title_home);
+//
+//                    return true;
+//                case R.id.navigation_dashboard:
+//                    mTextMessage.setText(R.string.title_dashboard);
+//                    return true;
+//                case R.id.navigation_notifications:
+//                    mTextMessage.setText(R.string.title_notifications);
+//                    Intent intent = new Intent(this, SettingsActivity.class);
+//                    startActivity(intent);
+//                    return true;
+//            }
+//            return false;
+//        }
+//    };
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        BottomNavigationView navView = findViewById(R.id.nav_view);
+//        mTextMessage = findViewById(R.id.message);
+//        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+//    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //loading the default fragment
+        loadFragment(new HomeFragment());
+
+        //getting bottom navigation view and attaching the listener
+        BottomNavigationView navigation = findViewById(R.id.nav_view);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+
+            case R.id.navigation_dashboard:
+                fragment = new DashboardFragment();
+                break;
+
+            case R.id.navigation_notifications:
+                fragment = new SettingsFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
+
+    public void calBtn(View view){
+
+    }
+
+    public void traBtn(View view){
+
+    }
+
+    public void budBtn(View view){
+
+    }
+
+    public void listBtn(View view){
+
+    }
+
+    public void exeBtn(View view){
+
+    }
+
+    public void diaBtn(View view){
+
+    }
+}
---
+++ app/src/main/java/com/example/myforethought/ui/login/dashboard/SettingsFragment.java
@@ -0,0 +1,19 @@
+package com.example.myforethought.ui.login.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.myforethought.R;
+
+public class SettingsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings, null);
+    }
+}
