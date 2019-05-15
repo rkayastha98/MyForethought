89d444f13a8a863cdb26f6099760a0b1d25c2e1d
rkayastha98 <rkayastha@wpi.edu>
2019-05-15 18:15:54 -0400

    more settings

---
+++ app/src/main/java/com/example/myforethought/ui/login/dashboard/Settings/AccountSettings.java
@@ -0,0 +1,18 @@
+package com.example.myforethought.ui.login.dashboard.Settings;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+import com.example.myforethought.ui.login.dashboard.SettingsFragment;
+
+public class AccountSettings extends SettingsFragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
